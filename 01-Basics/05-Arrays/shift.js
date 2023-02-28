a9613cc24189733d1e68d4e66996f10301505c0d
Amandeep Singh <wizzyt2222@gmail.com>
2023-02-28 17:09:05 +0530

    changed the folders

---
+++ 01-Basics/05-Arrays/shift.js
@@ -0,0 +1,7 @@
+// manipulating arrays with shift
+ var ourArray = ["YOYO",12,123342,"IDK MAN"]
+ // shift removes the first element
+
+ourArray.shift()
+console.log(ourArray) 
+// we can also catch it
---
+++ 01-Basics/06-Functions/hellofunc.js
@@ -0,0 +1,16 @@
+// functions allow us to write reusable codes
+
+function greeter (name){
+    console.log("Hello,",name)
+}
+
+greeter("Aman")
+
+
+// passing in arguments of int
+
+function Adder(a,b){
+    console.log(a+b);
+}
+
+Adder(1,2)
