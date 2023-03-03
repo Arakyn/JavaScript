8f968261df7fad8755985a2ae61de89ba6f27b0e
Amandeep Singh <wizzyt2222@gmail.com>
2023-03-03 23:31:40 +0530

    moree learning

--- 02-WebDevWithJS/03-BlackJack/game.js
+++ 02-WebDevWithJS/03-BlackJack/game.js
@@ -1,16 +1,35 @@
-function startGame (){
-    let firstcard = 21;
-let secondcard = 11;
+let firstcard = 10;
+let secondcard = 3;
+
+let cardsArray = [firstcard,secondcard]
 let hasBlackJack = false;
 let sum = firstcard + secondcard;
 let isAlive = true;
 let htmlMessage = document.getElementById("message-el")
 
-let cards = document.getElementById("cards-el")
-let sumHTML = document.getElementById("sum-el")
-cards.textContent = firstcard   + "  ,  " + secondcard
+let cards = document.querySelector("#cards-el")
+let sumHTML = document.querySelector("#sum-el")
+
+
 
 let message = "";
+
+
+function newCard(){
+    
+let card = 8;
+cardsArray.push(8)
+sum += card
+
+    renderGame()
+}
+
+function startGame (){
+    renderGame()
+}
+function renderGame (){
+
+   
    
 
 if (sum<21){
@@ -29,7 +48,10 @@ if (sum<21){
 } 
 htmlMessage.textContent = message;
 
+
 console.log(message)
 console.log(isAlive)
 console.log(hasBlackJack)
+cards.textContent =  cardsArray[0] + " "+ cardsArray[1] + " "
+sumHTML.textContent = "  "+ sum
 }
