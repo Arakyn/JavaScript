03efdd2405382ae088259f21010f2ffb7d42667f
Amandeep Singh <wizzyt2222@gmail.com>
2023-03-03 22:52:05 +0530

    lol

--- 02-WebDevWithJS/03-BlackJack/game.js
+++ 02-WebDevWithJS/03-BlackJack/game.js
@@ -1,11 +1,18 @@
-let firstcard = 10;
+function startGame (){
+    let firstcard = 21;
 let secondcard = 11;
 let hasBlackJack = false;
 let sum = firstcard + secondcard;
 let isAlive = true;
+let htmlMessage = document.getElementById("message-el")
+
+let cards = document.getElementById("cards-el")
+let sumHTML = document.getElementById("sum-el")
+cards.textContent = firstcard   + "  ,  " + secondcard
 
 let message = "";
    
+
 if (sum<21){
     message = "Do you want to draw a new card? 🙃"
    
@@ -20,5 +27,9 @@ if (sum<21){
     isAlive = false;
  
 } 
+htmlMessage.textContent = message;
 
 console.log(message)
+console.log(isAlive)
+console.log(hasBlackJack)
+}
